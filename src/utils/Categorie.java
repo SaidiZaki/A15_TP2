f10e6ddb95a417b43575b3b3d6a3bce035fdfaab
Mac sosso <baudrymbome@gmail.com>
2021-10-17 19:12:14 -0400

    update

--- src/utils/Categorie.java
+++ src/utils/Categorie.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package utils;
 
 import org.apache.http.client.utils.URIBuilder;
 
-/**
- *
- * @author adams
- */
+
 public class Categorie {
     
      public static String getCategorie() {
